0e6377f110b9431d9a3f4ce0d6e4657a11b46927
JonatanRoaCarrasco97 <jon.roa@duocuc.cl>
2025-01-29 14:25:06 -0300

    Revert "."
    This reverts commit f3db1293cb2f2e1fb0d93538aca97853efa5b2dd.

--- src/app/page/login/login.module.ts
+++ src/app/page/login/login.module.ts
@@ -1,18 +1,22 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { IonicModule } from '@ionic/angular';
+
 import { LoginPageRoutingModule } from './login-routing.module';
+
 import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule.forRoot(), // Forma correcta de importar el módulo de Ionic
+    IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Agrega esta línea
+  declarations: [LoginPage]
 })
-export class LoginPageModule {}
+export class LoginPageModule {
+  
+}
